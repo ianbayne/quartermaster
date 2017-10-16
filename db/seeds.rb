910e338df987dcb717c7a4275993e9efaf7d39e1
Ian Bayne <ianbayne@gmail.com>
2017-10-16 16:54:32 +0900

    Add user and equipment to seeds, still need to add contracts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Making seeds"
+
+6.times do
+  user = User.create(email: Faker::Internet.email, password: "123456")
+  adjective = %w(new old crappy like-new).sample
+  category = ["tent", "stove", "cooking goods", "sleeping bag"].sample
+  equipment = Equipment.new(name: "#{adjective} #{category}", description: Faker::Lorem.sentence, category: category, photo: nil)
+  equipment.user = user
+  equipment.save
+
+  # contract = Contract.new()
+end
+
+
+
+puts "Seeds finished!"
