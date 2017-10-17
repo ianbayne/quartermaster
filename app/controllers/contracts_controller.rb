78b1b413be9d9de396ac37ced6ad3df71141a696
kazuya nakajima <uniss010@gmail.com>
2017-10-17 18:27:29 +0900

    add basic create contract feature

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,25 @@
 class ContractsController < ApplicationController
+  def new
+    @equipment = Equipment.find(params[:equipment_id])
+    @contract = Contract.new
+  end
+
+  def create
+    @equipment = Equipment.find(params[:equipment_id])
+    @contract = Contract.new(contract_params)
+    @contract.equipment = @equipment
+    if @contract.save
+      redirect_to user_path(params[:contract][:user_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(
+      "start_time(1i)", "start_time(2i)", "start_time(3i)", 
+      "end_time(1i)", "end_time(2i)", "end_time(3i)", :user_id)
+  end
 end
