19bdff4eee227920ba75c9e1f1632dc7a4fc15aa
kazuya nakajima <uniss010@gmail.com>
2017-10-17 19:06:25 +0900

    add new columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017055445) do
+ActiveRecord::Schema.define(version: 20171017100142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 20171017055445) do
     t.string "last_name"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "user_name"
+    t.string "location"
+    t.boolean "host", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
