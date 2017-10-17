4ed1922e3ce46e506b7aa718a41ef20345b3bd20
kazuya nakajima <uniss010@gmail.com>
2017-10-17 14:56:13 +0900

    add new migration file to add price to equipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017033358) do
+ActiveRecord::Schema.define(version: 20171017055445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20171017033358) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
