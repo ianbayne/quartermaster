09367ec9874f7f8a81d1ae844fc15611aac48a64
Ian Bayne <ianbayne@gmail.com>
2017-10-17 19:47:42 +0900

    Complete edit for equipment in user profile

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -36,6 +36,13 @@ class EquipmentController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def destroy
+    # @user = User.find(params[:id])
+    # @equipment = Equipment.find(params[:id])
+    # @equipment.destroy
+    # redirect_to user_path(@user)
+  end
+
   private
 
   def equipment_params
