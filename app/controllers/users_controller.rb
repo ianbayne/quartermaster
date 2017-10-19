f12cf6e1b89a3ad38152236fa5c280539ad1e49b
kazuya nakajima <uniss010@gmail.com>
2017-10-19 13:05:30 +0900

    update user sign with location in and add user update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,26 @@ class UsersController < ApplicationController
     @user = current_user
     @contracts = @user.contracts.order(end_time: :asc)
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    
+    if @user.update(user_params)
+      flash[:success] = "Your profile was updated correctly."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :location)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :contracts
   has_many :equipment
 
+  LOCATION = [
+  "Adachi", "Arakawa", "Bunkyo", "Chiyoda", "Chuo", "Edogawa", 
+  "Itabashi", "Katsushika", "Kita", "Kōtō", "Meguro", "Minato", "Nakano", "Nerima", 
+  "Ota", "Setagaya", "Shibuya", "Shinagawa", "Shinjuku", "Suginami", "Sumida", 
+  "Taito", "Toshima"].map { |ward| ward + ", Tokyo"  }
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
